6cdaa94e50e45eebb0dcb54532ef722076f580cd
nahian255 <nahimn253@gmail.com>
2022-09-03 08:33:14 +0600

    internationalr news item done

--- new.js
+++ new.js
@@ -1,13 +1,13 @@
 
 
+
+// BreakingNewss.....
 const loadNews = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/news/category/01")
     const data = await res.json();
     return data;
     // console.log(data.data);
 }
-
-// BreakingNewss.....
 
 const displayBreakingNews = async () => {
     const data = await loadNews();
@@ -46,7 +46,6 @@ const loadRegularNews = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/news/category/02")
     const data = await res.json();
     return data;
-    // console.log(data.data);
 }
 const displayRegularNews = async () => {
     const data = await loadRegularNews();
@@ -82,6 +81,40 @@ const displayRegularNews = async () => {
 }
 // International news......
 
+const loadInternationalNews = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/03")
+    const data = await res.json();
+    return data;
+}
+
+const displayInternationalNews = async () => {
+    const data = await loadInternationalNews();
+    const findArry = data.data;
+    const cardPart = document.getElementById('card-part');
+    cardPart.textContent = '';
+    findArry.forEach(element => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+                <div class="card card-side bg-base-100 shadow-xl p-4 gap-3">
+                    <div class="w-2/6">
+                       <figure><img src="${element.image_url}" alt="Movie"></figure>
+                     </div>
+                    <div class="card-body w-4/6 ">
+                        <h2 class="card-title">${element.title}</h2>
+                        <p>${element.details + "..."}</p>
+                        <div class="card-actions justify-end">
+                           <div>
+                               <p> ${element.author.name}</p>
+                               <img class="w-6 h-4" src="${element.author.img}" alt="Movie">
+                           </div>
+                           <P class="mx-9">${element.rating.number}M</p>
+                           <button class="btn btn-primary">Read More</button>
+                        </div>
+                    </div>
+                </div> `;
+        cardPart.appendChild(div);
+    });
+}
 
 
 
