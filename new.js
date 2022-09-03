c07e6d1726ad203463947cad4fccebda652518f6
nahian255 <nahimn253@gmail.com>
2022-09-03 08:48:14 +0600

    almost show all news

--- new.js
+++ new.js
@@ -187,16 +187,72 @@ const displayEntertainmentNews = async () => {
 }
 // Culture.............
 const loadCultureNews = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/news/category/03")
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/06")
     const data = await res.json();
     return data;
 }
 
+const displayCultureNews = async () => {
+    const data = await loadCultureNews();
+    const findArry = data.data;
+    const cardPart = document.getElementById('card-part');
+    cardPart.textContent = '';
+    findArry.forEach(element => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+                <div class="card card-side bg-base-100 shadow-xl p-4 gap-3">
+                    <div class="w-2/6">
+                       <figure><img src="${element.image_url}" alt="Movie"></figure>
+                     </div>
+                    <div class="card-body w-4/6 ">
+                        <h2 class="card-title">${element.title}</h2>
+                        <p>${element.details + "..."}</p>
+                        <div class="card-actions justify-end">
+                           <div>
+                               <p> ${element.author.name}</p>
+                               <img class="w-6 h-4" src="${element.author.img}" alt="Movie">
+                           </div>
+                           <P class="mx-9">${element.rating.number}M</p>
+                           <button class="btn btn-primary">Read More</button>
+                        </div>
+                    </div>
+                </div> `;
+        cardPart.appendChild(div);
+    });
+}
 // Art
 const loadArtNews = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/news/category/03")
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/07")
     const data = await res.json();
     return data;
+}
+const displayArtNews = async () => {
+    const data = await loadArtNews();
+    const findArry = data.data;
+    const cardPart = document.getElementById('card-part');
+    cardPart.textContent = '';
+    findArry.forEach(element => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+                <div class="card card-side bg-base-100 shadow-xl p-4 gap-3">
+                    <div class="w-2/6">
+                       <figure><img src="${element.image_url}" alt="Movie"></figure>
+                     </div>
+                    <div class="card-body w-4/6 ">
+                        <h2 class="card-title">${element.title}</h2>
+                        <p>${element.details + "..."}</p>
+                        <div class="card-actions justify-end">
+                           <div>
+                               <p> ${element.author.name}</p>
+                               <img class="w-6 h-4" src="${element.author.img}" alt="Movie">
+                           </div>
+                           <P class="mx-9">${element.rating.number}M</p>
+                           <button class="btn btn-primary">Read More</button>
+                        </div>
+                    </div>
+                </div> `;
+        cardPart.appendChild(div);
+    });
 }
 // ALL news........
 
