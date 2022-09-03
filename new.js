372ecb5b7caa007350837ed769b09f98d476045f
nahian255 <nahimn253@gmail.com>
2022-09-03 08:39:16 +0600

    sports news show

--- new.js
+++ new.js
@@ -116,9 +116,61 @@ const displayInternationalNews = async () => {
     });
 }
 
+// Sports.....
+const loadSportsNews = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/04")
+    const data = await res.json();
+    return data;
+}
 
+const displaySportsNews = async () => {
+    const data = await loadSportsNews();
+    const findArry = data.data;
+    const cardPart = document.getElementById('card-part');
+    cardPart.textContent = '';
+    findArry.forEach(element => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+                <div class="card card-side bg-base-100 shadow-xl p-4 gap-3">
+                    <div class="w-2/6">
+                       <figure><img src="${element.image_url}" alt="Movie"></figure>
+                     </div>
+                    <div class="card-body w-4/6 ">
+                        <h2 class="card-title">${element.title}</h2>
+                        <p>${element.details + "..."}</p>
+                        <div class="card-actions justify-end">
+                           <div>
+                               <p> ${element.author.name}</p>
+                               <img class="w-6 h-4" src="${element.author.img}" alt="Movie">
+                           </div>
+                           <P class="mx-9">${element.rating.number}M</p>
+                           <button class="btn btn-primary">Read More</button>
+                        </div>
+                    </div>
+                </div> `;
+        cardPart.appendChild(div);
+    });
+}
+// Entertainment.........
+const loadEntertainmentNews = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/03")
+    const data = await res.json();
+    return data;
+}
+// Culture.............
+const loadCultureNews = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/03")
+    const data = await res.json();
+    return data;
+}
 
-
+// Art
+const loadArtNews = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/category/03")
+    const data = await res.json();
+    return data;
+}
+// ALL news........
 
 
 
